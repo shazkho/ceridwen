4f60317f7d9c7e8836f1eb8852bd48b1b6784924
George Shazkho <shazkho@gmail.com>
2019-06-04 10:10:35 -0400

    IDE Helper y Collective para Laravel 5.8

--- config/app.php
+++ config/app.php
@@ -175,6 +175,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Providers adicionales agregados para el proyecto Ceridwen
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +231,12 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Aliases agregados para el proyecto Ceridwen
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
